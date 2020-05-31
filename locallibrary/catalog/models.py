6580d215cc96e0a2a0c9f95ce31eb6c5c531c75b
Jiwon Jung <jungkt0x@naver.com>
2020-06-01 02:40:20 +0900

    complete tutorial 3,4

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -15,18 +15,23 @@ class Language(models.Model):
 
 #class book(models.Model):
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    summary = models.CharField(max_length=1000, help_text='Enter a brief description of the book')
+    title = models.CharField(max_length=200, verbose_name='제목')
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, verbose_name='저자')
+    summary = models.CharField(max_length=1000, help_text='Enter a brief description of the book', verbose_name='요약')
     isbn = models.CharField(max_length=13)
-    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
-    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book', verbose_name='장르')
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True, verbose_name='언어')
 
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
         
 import uuid #Required for unique book instances
 
@@ -64,7 +69,7 @@ class Author(models.Model):
     date_of_death = models.DateField('Died', null=True, blank=True)
 
     class Meta:
-        ordering = ['last_name', 'first_name']
+        ordering = ['last_name' , 'first_name']
 
     def get_absolute_url(self):
         return reverse('author-detail', args=[str(self.id)])
